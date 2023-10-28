6c565d0472566411a412624ea9fb06f8e94be2a6
Felipe Moreno <felipe@flpm.dev>
2023-10-28 16:47:48 -0400

    updated design for book info pages

--- pages/books/info/[book].js
+++ pages/books/info/[book].js
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Layout from "../../../components/layout";
 import Section from "../../../components/section";
 import Link from "next/link";
-import { openSans } from "../../../components/fonts";
+import { openSans, iADuoItalic } from "../../../components/fonts";
 import { getBookData, getAllIds } from "../../../lib/books";
 import { useRouter } from "next/router";
 import moment from "moment";
@@ -47,6 +47,12 @@ const relatedLinkList = (related) => {
 export default function Page({ data }) {
   const router = useRouter();
   let durationString = "";
+  const parts = data.content.split("---");
+  const description = parts[0];
+  let note = null;
+  if (parts.length > 1) {
+    note = parts[1];
+  }
   if (data.source === "Audible") {
     const seconds = data.length / 1000;
     const duration = moment.duration(seconds, "seconds");
@@ -81,7 +87,7 @@ export default function Page({ data }) {
         spaceAfter={2}
         className="flex flex-col "
       >
-        <div className={`${openSans.className}`}>
+        <div className={`${openSans.className} pl-[15px]`}>
           <div className="text-6xl font-semibold">{data.title}</div>
           {data.subtitle == undefined ? null : (
             <div className={`text-5xl font-light pt-2`}>{data.subtitle}</div>
@@ -129,43 +135,53 @@ export default function Page({ data }) {
           </div>
 
           {data.first_edition ? (
-            <p className="text-xl font-bold">
+            <p className="text-xl text-gray-400">
               <span className=""></span>
             </p>
           ) : null}
           {data.signed ? (
-            <p className="text-xl font-bold">
+            <p className="text-xl text-gray-400">
               {data.first_edition ? "First edition. " : null}
-              {data.signed ? "Signed copy. " : null}
+              {data.signed ? `Signed book.` : null}
             </p>
           ) : null}
         </div>
-        {/* <div className="pt-6 font-semibold">
-          {data.read_status !== undefined ? (
-            <p className="text-xl pt-6">
-              <span className="">
-                I {data.read_status} this {data.type}
-                {data.multiple_reads ? " multiple times" : null}
-                {data.recommendation_status === true
-                  ? " and I recommend it."
-                  : data.recommendation_status === false
-                  ? " and I do not recommend it."
-                  : "."}
-              </span>
-            </p>
-          ) : null}
-        </div> */}
+        <div className={`markdown-content text-xl`}>
+          <div className="pt-12 pl-[15px]">
+            <div className={`markdown-content-part text-xl`}>
+              {note != null ? (
+                <ReactMarkdown
+                  components={{
+                    a: ({ children, href }) => {
+                      return <Link href={href}>{children}</Link>;
+                    },
+                  }}
+                >
+                  {note}
+                </ReactMarkdown>
+              ) : (
+                <p>I have not written about this book yet.</p>
+              )}
+            </div>
+          </div>
 
-        <div className="markdown-content text-xl pt-12">
-          <ReactMarkdown
-            components={{
-              a: ({ children, href }) => {
-                return <Link href={href}>{children}</Link>;
-              },
-            }}
-          >
-            {data.content}
-          </ReactMarkdown>
+          {/* <p className="pt-12 font-semibold">From the publishers: </p> */}
+          <div className="pt-12">
+            <div
+              className={`markdown-content-part pl-[15px] pr-6 py-4 border-l-8 border-l-gray-300 bg-gray-100`}
+            >
+              <p>Description from the publisher:</p>
+              <ReactMarkdown
+                components={{
+                  a: ({ children, href }) => {
+                    return <Link href={href}>{children}</Link>;
+                  },
+                }}
+              >
+                {description}
+              </ReactMarkdown>
+            </div>
+          </div>
 
           {data.related !== undefined &&
             data.related.length > 0 &&
